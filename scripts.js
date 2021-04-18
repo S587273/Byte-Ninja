c388f301831f3100e9fbe520fe0775e33a0c51fe
Cornmos <72299677+Cornmos@users.noreply.github.com>
2021-04-19 00:51:29 +0930

    implemented Database objects

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
-class Destinations{
-    constructor(name,description,price){
+class Product{
+    constructor(name,description,price,pic,onsale){
         this.name = name;
         this.description = description;
         this.price = price;
+        this.pic = pic
+        this.onsale= onsale
     }
 }
 
-var dest1 = new Destinations("Bora Bora","Bora Bora was discovered by Captain James Cook in 1769. It’s one of the most secluded islands on Earth. It’s halfway between Australia and the United States.",2800);
-var dest2 = new Destinations("Bali","While Bali is the name of the island, it is also a province that includes a handful of smaller islands congregated off the main island’s southeast coast.",850);
-var dest3 = new Destinations("El Nido","El Nido is arguably one of the most popular resort destinations in the Philippines",192);
-var destlist = [dest1,dest2,dest3] 
-var taglist = ["tag1","tag2","tag3"]
+var product1 = new Product("AMD Ryzen 9 5900X Desktop CPU","A formidable upgrade to your desktop system, the AMD Ryzen 9 5900X Desktop CPU offers powerful computing performance to handle heavy-load applications and multi-threaded tasks, such as content creation, video encoding, and gaming.",859,"pic1.jpg",true);
+var product2 = new Product("Intel Core i7-10700k Boxed Processor","The Intel Core i7- 10700K Boxed Processor offers fast performance and advanced technologies, including Turbo Boost 2.0, Hyper-Threading, and Virtualization, making it a formidable upgrade for your compatible system or a great option for building a new setup.",539,"pic2.jpg",false);
+var product3 = new Product("AMD Ryzen 9 5950X Desktop CPU","Keeping the pace with intensive multi-processing applications and hardcore video gaming, the AMD Ryzen 9 5950X Desktop CPU delivers excellent computing performance without sacrificing power-efficiency.",1249,"pic3.jpg",false);
+var product4 = new Product("ADATA XPG Spectrix D60G 16GB Memory Kit","Satisfying e-sports players with that they need, the ADATA XPG Spectrix D60G DDR4 RGB 3600MHz 16GB Memory Kit (2x8GB) features a wide frequency range for fluid overclocking performance and a unique RGB strip to light up the battlefield.",209,"pic4.jpg",true);
+var product7 = new Product("ADATA Ultimate SU650 120GB Internal SSD","The ADATA Ultimate SU650 Internal SSD delivers read and write speeds of up to 520 and 420 MB/s* respectively, making it perfect for relocating large files like Blu-ray movies, gaming software, surveillance footage, and more.",56,"pic7.jpg",true);
+var productlist = [product1,product2,product3,product4,product7]
+var taglist = ["tag1","tag2","tag3","tag4","tag7"]
+var piclist = ["pic1","pic2","pic3","pic4","pic7"]
 
-function fun1()
-{   
-    for( var i=0; destlist.length;i++)
+function index()
+{
+    for(var i=0; productlist.length;i++){
     document.getElementById(taglist[i]).innerHTML=
-    "<h3>"+destlist[i].name+"</h3><br>"+
-    "<p>"+destlist[i].description+"</p><br>"+
-    "<p>$"+destlist[i].price+"</p><br>";
+    "<h3>"+productlist[i].name+"</h3><br>"+
+    "<p>"+productlist[i].description+"</p><br>"+
+    "<p>$"+productlist[i].price+"</p><br>";
+    document.getElementById(piclist[i]).src=productlist[i].pic;
+}
+}
+
+function on_sale()
+{
+    for(var i=0; productlist.length;i++){
+      if(productlist[i].onsale==true){
+        productprice=productlist[i].price*.9
+        document.getElementById(taglist[i]).innerHTML=
+        "<h3>"+productlist[i].name+"</h3><br>"+
+        "<p>"+productlist[i].description+"</p><br>"+
+        "<p>Special $"+Math.ceil(productprice)+"</p><br>";
+        document.getElementById(piclist[i]).src=productlist[i].pic;
+}
+}
 }
 
 function formValidation() {
@@ -36,12 +57,13 @@ function formValidation() {
                         }
                     }
                 }
+                return false;
             }
-        
-    
-    return false;
 
-    
+
+
+
+
 function allLetter(uname) {
     var letters = /^[A-Za-z]+$/;
     if (uname.value.match(letters)) {
