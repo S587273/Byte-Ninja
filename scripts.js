fc5859ad6f729346c33658c8551d69d20e1bfe96
Ben Carruth <79821597+S587273@users.noreply.github.com>
2021-04-24 15:51:16 +0930

    Update scripts.js
    fixed variables not loading in onsale.html

--- scripts.js
+++ scripts.js
@@ -9,8 +9,8 @@ class Product{
 }
 
 var product1 = new Product("AMD Ryzen 9 5900X Desktop CPU","A formidable upgrade to your desktop system, the AMD Ryzen 9 5900X Desktop CPU offers powerful computing performance to handle heavy-load applications and multi-threaded tasks, such as content creation, video encoding, and gaming.",859,"pic1.jpg",true);
-var product2 = new Product("Intel Core i7-10700k Boxed Processor","The Intel Core i7- 10700K Boxed Processor offers fast performance and advanced technologies, including Turbo Boost 2.0, Hyper-Threading, and Virtualization, making it a formidable upgrade for your compatible system or a great option for building a new setup.",539,"pic2.jpg",false);
-var product3 = new Product("AMD Ryzen 9 5950X Desktop CPU","Keeping the pace with intensive multi-processing applications and hardcore video gaming, the AMD Ryzen 9 5950X Desktop CPU delivers excellent computing performance without sacrificing power-efficiency.",1249,"pic3.jpg",false);
+var product2 = new Product("Intel Core i7-10700k Boxed Processor","The Intel Core i7- 10700K Boxed Processor offers fast performance and advanced technologies, including Turbo Boost 2.0, Hyper-Threading, and Virtualization, making it a formidable upgrade for your compatible system or a great option for building a new setup.",539,"pic2.jpg",true);
+var product3 = new Product("AMD Ryzen 9 5950X Desktop CPU","Keeping the pace with intensive multi-processing applications and hardcore video gaming, the AMD Ryzen 9 5950X Desktop CPU delivers excellent computing performance without sacrificing power-efficiency.",1249,"pic3.jpg",true);
 var product4 = new Product("ADATA XPG Spectrix D60G 16GB Memory Kit","Satisfying e-sports players with that they need, the ADATA XPG Spectrix D60G DDR4 RGB 3600MHz 16GB Memory Kit (2x8GB) features a wide frequency range for fluid overclocking performance and a unique RGB strip to light up the battlefield.",209,"pic4.jpg",true);
 var product5 = new Product("DDR4 3000MHz Vengeance LPX White","Vengeance LPX memory is designed for high-performance overclocking. The heatspreader is made of pure aluminum for faster heat dissipation, and the eight-layer PCB helps manage heat and provides superior overclocking headroom",219,"pic5.jpg",false);
 var product6 = new Product("Team 16GB Elite 2666MHz DDR4 RAM","Team 16GB (1x16GB) TED416G2666C1902 Elite 2666MHz DDR4 RAM",99,"pic6.jpg",false);
@@ -22,7 +22,7 @@ var product11 = new Product("Asus GeForce 2GB GDDR5 Video Card","ASUS GeForce GT
 var product12 = new Product("Gigabyte GeForce RTX 2060 R2.0 6G OC Graphics Card","GeForce RTX™ 2060 OC 6G(2.0)GeForce RTX™ 2060 OC 6G(2.0)GeForce RTX™ 2060 OC 6G(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)",799,"pic12.jpg",false);
 var product13 = new Product("Asus Prime B450M-A mATX Motherboard","Fan Xpert 2+: Flexible controls for ultimate cooling and silence, plus GPU-temperature sensing for cooler gaming. Ultrafast connectivity: Supreme flexibility with USB 3.1 Gen 2 and native M.2. ASUS Aura Sync header: Onboard connector for RGB LED strips, easily synced with an ever-growing portfolio of Aura Sync-capable hardware.",105,"pic13.jpg",false);
 var product14 = new Product("Asus Prime Z390M Plus Motherboard","OptiMem II: Careful routing of traces and vias, plus ground layer optimizations to preserve signal integrity for improved memory overclocking. Enhanced power solution: Premium components provide better power efficiency. Industry-leading cooling options: Comprehensive controls for fans and AIO pump, via Fan Xpert 4 or the acclaimed UEFI.",209,"pic14.jpg",false);
-var product15 = new Product("","AM4 socket: Ready for 2nd and 3rd Gen AMD Ryzen processors to maximize connectivity and speed with up to two M.2 Drives, USB 3.2 Gen2 and AMD StoreMI. Aura Sync RGB: ASUS-exclusive Aura Sync RGB lighting, including RGB headers and addressable Gen 2 GRB headers. Optimal Power Solution: 12+4 power stages with ProCool II power connector, alloy chokes and durable capacitors to support multi-core processors.",415,"pic15.jpg",false);
+var product15 = new Product("Asus Strix x570E Gaming Motherboard","AM4 socket: Ready for 2nd and 3rd Gen AMD Ryzen processors to maximize connectivity and speed with up to two M.2 Drives, USB 3.2 Gen2 and AMD StoreMI. Aura Sync RGB: ASUS-exclusive Aura Sync RGB lighting, including RGB headers and addressable Gen 2 GRB headers. Optimal Power Solution: 12+4 power stages with ProCool II power connector, alloy chokes and durable capacitors to support multi-core processors.",415,"pic15.jpg",true);
 var productlist = [product1,product2,product3,product4,product5,product6,product7,product8,product9,product10,product11,product12,product13,product14,product15]
 var taglist = ["tag1","tag2","tag3","tag4","tag5","tag6","tag7","tag8","tag9","tag10","tag11","tag12","tag13","tag14","tag15"]
 var piclist = ["pic1","pic2","pic3","pic4","pic5","pic6","pic7","pic8","pic9","pic10","pic11","pic12","pic13","pic14","pic15"]
@@ -38,7 +38,7 @@ function index()
 }
 }
 
-function on_sale()
+function onsale()
 {
     
     for(var i=0; productlist.length;i++){
