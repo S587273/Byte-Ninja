bcec059e7b4de2f22a873fd9e42f7de4b7865d5f
Cornmos <72299677+Cornmos@users.noreply.github.com>
2021-05-02 17:58:59 +0930

    Form Validation
    Form now shoes if data entered is empty and if the 2 passwords entered are the same or different. Note that if a different password is entered on the second input the error is different. The Functions are in place if you want to add more regular expressions to the form validation. Email now correctly checks if the email format is correct.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-HIT226 GROUP D/HD BYTE NINJA.COM WEBPAGE
+# Information-Cards
+information cards week 5 s587273
--- scripts.js
+++ scripts.js
@@ -9,8 +9,8 @@ class Product{
 }
 
 var product1 = new Product("AMD Ryzen 9 5900X Desktop CPU","A formidable upgrade to your desktop system, the AMD Ryzen 9 5900X Desktop CPU offers powerful computing performance to handle heavy-load applications and multi-threaded tasks, such as content creation, video encoding, and gaming.",859,"pic1.jpg",true);
-var product2 = new Product("Intel Core i7-10700k Boxed Processor","The Intel Core i7- 10700K Boxed Processor offers fast performance and advanced technologies, including Turbo Boost 2.0, Hyper-Threading, and Virtualization, making it a formidable upgrade for your compatible system or a great option for building a new setup.",539,"pic2.jpg",true);
-var product3 = new Product("AMD Ryzen 9 5950X Desktop CPU","Keeping the pace with intensive multi-processing applications and hardcore video gaming, the AMD Ryzen 9 5950X Desktop CPU delivers excellent computing performance without sacrificing power-efficiency.",1249,"pic3.jpg",true);
+var product2 = new Product("Intel Core i7-10700k Boxed Processor","The Intel Core i7- 10700K Boxed Processor offers fast performance and advanced technologies, including Turbo Boost 2.0, Hyper-Threading, and Virtualization, making it a formidable upgrade for your compatible system or a great option for building a new setup.",539,"pic2.jpg",false);
+var product3 = new Product("AMD Ryzen 9 5950X Desktop CPU","Keeping the pace with intensive multi-processing applications and hardcore video gaming, the AMD Ryzen 9 5950X Desktop CPU delivers excellent computing performance without sacrificing power-efficiency.",1249,"pic3.jpg",false);
 var product4 = new Product("ADATA XPG Spectrix D60G 16GB Memory Kit","Satisfying e-sports players with that they need, the ADATA XPG Spectrix D60G DDR4 RGB 3600MHz 16GB Memory Kit (2x8GB) features a wide frequency range for fluid overclocking performance and a unique RGB strip to light up the battlefield.",209,"pic4.jpg",true);
 var product5 = new Product("DDR4 3000MHz Vengeance LPX White","Vengeance LPX memory is designed for high-performance overclocking. The heatspreader is made of pure aluminum for faster heat dissipation, and the eight-layer PCB helps manage heat and provides superior overclocking headroom",219,"pic5.jpg",false);
 var product6 = new Product("Team 16GB Elite 2666MHz DDR4 RAM","Team 16GB (1x16GB) TED416G2666C1902 Elite 2666MHz DDR4 RAM",99,"pic6.jpg",false);
@@ -22,7 +22,7 @@ var product11 = new Product("Asus GeForce 2GB GDDR5 Video Card","ASUS GeForce GT
 var product12 = new Product("Gigabyte GeForce RTX 2060 R2.0 6G OC Graphics Card","GeForce RTX™ 2060 OC 6G(2.0)GeForce RTX™ 2060 OC 6G(2.0)GeForce RTX™ 2060 OC 6G(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)GeForce RTX™ 2060 OC(2.0)",799,"pic12.jpg",false);
 var product13 = new Product("Asus Prime B450M-A mATX Motherboard","Fan Xpert 2+: Flexible controls for ultimate cooling and silence, plus GPU-temperature sensing for cooler gaming. Ultrafast connectivity: Supreme flexibility with USB 3.1 Gen 2 and native M.2. ASUS Aura Sync header: Onboard connector for RGB LED strips, easily synced with an ever-growing portfolio of Aura Sync-capable hardware.",105,"pic13.jpg",false);
 var product14 = new Product("Asus Prime Z390M Plus Motherboard","OptiMem II: Careful routing of traces and vias, plus ground layer optimizations to preserve signal integrity for improved memory overclocking. Enhanced power solution: Premium components provide better power efficiency. Industry-leading cooling options: Comprehensive controls for fans and AIO pump, via Fan Xpert 4 or the acclaimed UEFI.",209,"pic14.jpg",false);
-var product15 = new Product("Asus Strix x570E Gaming Motherboard","AM4 socket: Ready for 2nd and 3rd Gen AMD Ryzen processors to maximize connectivity and speed with up to two M.2 Drives, USB 3.2 Gen2 and AMD StoreMI. Aura Sync RGB: ASUS-exclusive Aura Sync RGB lighting, including RGB headers and addressable Gen 2 GRB headers. Optimal Power Solution: 12+4 power stages with ProCool II power connector, alloy chokes and durable capacitors to support multi-core processors.",415,"pic15.jpg",true);
+var product15 = new Product("","AM4 socket: Ready for 2nd and 3rd Gen AMD Ryzen processors to maximize connectivity and speed with up to two M.2 Drives, USB 3.2 Gen2 and AMD StoreMI. Aura Sync RGB: ASUS-exclusive Aura Sync RGB lighting, including RGB headers and addressable Gen 2 GRB headers. Optimal Power Solution: 12+4 power stages with ProCool II power connector, alloy chokes and durable capacitors to support multi-core processors.",415,"pic15.jpg",false);
 var productlist = [product1,product2,product3,product4,product5,product6,product7,product8,product9,product10,product11,product12,product13,product14,product15]
 var taglist = ["tag1","tag2","tag3","tag4","tag5","tag6","tag7","tag8","tag9","tag10","tag11","tag12","tag13","tag14","tag15"]
 var piclist = ["pic1","pic2","pic3","pic4","pic5","pic6","pic7","pic8","pic9","pic10","pic11","pic12","pic13","pic14","pic15"]
@@ -38,7 +38,7 @@ function index()
 }
 }
 
-function onsale()
+function on_sale()
 {
     
     for(var i=0; productlist.length;i++){
@@ -53,90 +53,105 @@ function onsale()
 }
 }
 
-function formValidation() {
-    var uname = document.registration.username;
-    var uadd = document.registration.address;
-    var uemail = document.registration.email;
-    var ValidateDateofBirth = document.registration.dateofbirth;
-    var ufsex = document.registration.fsex;
-        if (allLetter(uname)) {
-            if (alphanumeric(uadd)) {
-                if (ValidateEmail(uemail)) {
-                    if (allnumeric(uzip)) {
-                        if (validsex(umsex, ufsex)) {}
-                            }
-                        }
-                    }
-                }
-                return false;
-            }
-
-
-
-
-
-function allLetter(uname) {
-    var letters = /^[A-Za-z]+$/;
-    if (uname.value.match(letters)) {
-        return true;
+function validate(){
+    var uname= document.getElementById("uName");
+    var address= document.getElementById("address");
+    var email= document.getElementById("email");
+    var DOB= document.getElementById("DOB");
+    var tel= document.getElementById("tel");
+    var pword=document.getElementById("pword");
+    var pword2=document.getElementById("pword2");
+    var noinput = "";
+    var ch_Uname = ch_Add = ch_Email = ch_DOB = ch_Pnum = ch_Pword = false
+    if (uname.value==noinput||uname.value==null)
+    {
+        uname.style.border="solid 3px red"
+        document.getElementById("lbluser").style.visibility="visible";
     }
-    else {
-        alert('Username must have alphabet characters only');
-        uname.focus();
-        return false;
+    else
+    { 
+        uname.style.border="solid 3px white"
+        document.getElementById("lbluser").style.visibility="hidden";
+        ch_Uname = true;
     }
-}
+    if (address.value==noinput||address.value==null)
+    {
+        address.style.border="solid 3px red"
+        document.getElementById("lbladdress").style.visibility="visible";
+    }
+    else
+    {   
+        address.style.border="solid 3px white"
+        document.getElementById("lbladdress").style.visibility="hidden";
+        ch_Add = true;
+    }
+    if (email.value==noinput||email.value==null)
+    {
+        email.style.border="solid 3px red"
+        document.getElementById("lblemail").style.visibility="visible";
+        
+    }
+    else
+    { 
+        email.style.border="solid 3px white"
+        document.getElementById("lblemail").style.visibility="hidden";
+        ch_Email = true;
+    }
+    if (DOB.value==noinput||DOB.value==null)
+    {
+        DOB.style.border="solid 3px red"
+        document.getElementById("lblDOB").style.visibility="visible";
+    }
+    else
+    { 
+        DOB.style.border="solid 3px white"
+        document.getElementById("lblDOB").style.visibility="hidden";
+        ch_DOB=true;
+    }
+    if (tel.value==noinput||tel.value==null)
+    {
+        tel.style.border="solid 3px red"
+        document.getElementById("lbltel").style.visibility="visible";
+    }
+    else
+    { 
+        tel.style.border="solid 3px white"
+        document.getElementById("lbltel").style.visibility="hidden";
+        ch_Pnum=true;
+    }
+    if (pword.value==noinput||pword.value==null)
+    {   
+        pword.style.border="solid 3px red"
+        document.getElementById("lblpword").style.visibility="visible";
+    }
+    else
+    { 
+        pword.style.border="solid 3px white"
+        document.getElementById("lblpword").style.visibility="hidden";
+    }
+    if (pword2.value==noinput||pword2.value==null)
+    {   
+        pword2.style.border="solid 3px red"
+        document.getElementById("lblpword2").style.visibility="visible";
+        document.getElementById("lblpword2").innerHTML="Invalid Input"
+    }
+    else if (pword2.value!=pword.value){
+        pword2.style.border="solid 3px red"
+        document.getElementById("lblpword2").innerHTML="Password don't match"
+        document.getElementById("lblpword2").style.visibility="visible";
 
-function alphanumeric(uadd) {
-    var letters = /^[0-9a-zA-Z]+$/;
-    if (uadd.value.match(letters)) {
-        return true;
     }
-    else {
-        alert('User address must have alphanumeric characters only');
-        uadd.focus();
-        return false;
+    else
+    { 
+        pword2.style.border="solid 3px white"
+        document.getElementById("lblpword2").style.visibility="hidden";
+        ch_Pword=true;
     }
-}
-
-function ValidateEmail(mail) {
-    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value)) {
-        return (true)
+    if(ch_Uname==true && ch_Add==true && ch_Email==true && ch_DOB==true && ch_Pnum==true && ch_Pword==true){
+        alert("form submitted")
     }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
-
-function ValidateDateofBirth (date) {
-    var pattern = /^([0-9]{2})-([0-9]{2})-([0-9]{4})$/;
-    if (pattern.test(str_input_date)) {
-        alert("valid date");
-        dateofbirth.focus()
-        return false;
+    else{
+        var ch_Uname = ch_Add = ch_Email = ch_DOB = ch_Pnum = ch_Pword = false
+        return
     }
-    return true;
-
-}
-
-function allnumeric(uphone) {
-    var numbers = /^[0-9]+$/;
-    if (uphone.value <1 (uphone)) {
-        return true;
-    }
-    else {
-        alert ("Enter Country Code");
-        uphone.focus();
-        return false;
-    }
-
-}
-
-function passid_validation(passid, mx, my) {
-    var passid_len = passid.value.length;
-    if (passid_len == 0 || passid_len >= my || passid_len < mx) {
-        alert("Password should not be empty / length be between " + mx + " to " + my);
-        passid.focus();
-        return false;
-    }
-    return true;
 }
